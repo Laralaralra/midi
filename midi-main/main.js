2e32f854d791a9cdcab785cbe827c03d463ab77e
Laralaralra <111282319+Laralaralra@users.noreply.github.com>
2023-10-31 09:35:29 -0300

    Add files via upload

---
+++ midi-main/main.js
@@ -0,0 +1,14 @@
+// Pom
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < 9) {
+    
+    listaDeTeclas[0].onclick = TocaSomPom;
+}
